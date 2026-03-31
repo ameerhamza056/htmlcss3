a6817d7a3ba2515b83f0c88581f5a0f397467170
ameerhamza056 <hamzaawaismughal@gmail.com>
2026-03-31 19:54:53 +0500

    Add files via upload

---
+++ src/Components/Html.jsx
@@ -0,0 +1,12 @@
+export default function Html(){
+  return(
+    <>
+      <div class="main"> 
+        <div class="x"></div>
+        <div class="y"></div>
+        <div class="z"></div>
+    </div>
+    <h1>Flag of Belguim</h1>
+    </>
+  )
+}
